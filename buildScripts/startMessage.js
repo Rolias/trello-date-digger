1b4f7c87933fa33488ea4897a240f2ffd0132de4
Tod Gentille <tod.gentille@gmail.com>
2019-03-11 17:54:35 -0700

    initial commit

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+const logger = require('../src/util/logger')
+// Showing both ways to disable an eslint rule. 
+/* eslint-disable no-console */
+logger.info('===================== Starting Up =====================')
