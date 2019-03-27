c51c27b4867d083c3f786c6ab237d934deb815c4
Tod Gentille <tod.gentille@gmail.com>
2019-03-27 13:29:08 -0700

    minor clean up and add readme

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ const moment = require('moment')
 
 const COMMISSIONED_LIST_ID = '5a43cacb866f58773c0262c5'
 const SHEET_ID = '1M9B7Rc5f8BD0eN8NIG39zkNJ0BTZdGP6k3608gEe-7A'
+// https://docs.google.com/spreadsheets/d/1M9B7Rc5f8BD0eN8NIG39zkNJ0BTZdGP6k3608gEe-7A
 
-const formatDate = (date) => {
-  return moment(date).format('YYYY-MM-DD')
-}
+const formatDate = (date) => moment(date).format('YYYY-MM-DD')
 
 const main = async () => {
   const trello = new Trello('/Users/tod-gentille/dev/node/ENV_VARS/trello.env.json')
@@ -16,8 +15,7 @@ const main = async () => {
   const {sheetOps} = gsweet
 
 
-  // const cardsFromList = await trello.getCardsOnList({id: COMMISSIONED_LIST_ID, options: {limit: 40}})
-  const cardsFromList = await trello.getCardsOnList({id: COMMISSIONED_LIST_ID})
+  const cardsFromList = await trello.getCardsOnList({id: COMMISSIONED_LIST_ID, options: {}})
   const cardList = []
   cardList.push(['Name', 'Oldest Data', 'Date card moved to Board', '# board moves found'])
   for (const card of cardsFromList) {
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Trello Date Digger
+
+Pull in all the cards on the Commissioned list on the Trello Proposal Board
+Find the oldest action date on a card, also find out if there is an action for moving the card to the board. That typically represents when it went from an idea to greenlit for a proposal.  It also writes the number of board moves found. If the number is not 0 or 1 it will require manual digging to determine how long it's been a proposal. 
+
+Written in response to a request by SBO 
